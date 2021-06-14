077c79251562896a89e6b269a492877e63698166
SanchesTest <57625804+SanchesTest@users.noreply.github.com>
2021-06-14 18:00:24 +0300

    t

---
+++ working-with-ready-code/script.js
@@ -0,0 +1,10 @@
+// Слайдер нативный JS
+// https://github.com/ganlanyuan/tiny-slider
+
+
+// Работа со свайпом
+// https://hammerjs.github.io/
+
+
+// Сборник HTML, CSS, JavaScript/jQuery компонентов
+// https://nisnom.com/
